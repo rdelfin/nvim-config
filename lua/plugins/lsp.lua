97e68d41ee22ae5e2450722ce3c7ee83fe0d72d6
Ricardo Delfin <ricdelfin@bloomberg.net>
2024-12-17 05:32:20 -0500

    Made blink.cmp less aggressive about showing autocomplete

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,4 +18,14 @@ return {
       },
     },
   },
+  {
+    "Saghen/blink.cmp",
+    opts = {
+      completion = {
+        trigger = {
+          show_on_insert_on_trigger_character = false,
+        },
+      },
+    },
+  },
 }
