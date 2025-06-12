0d2f046bf75f46649b2535facfd6adb4c44f6ecd
Ricardo Delfin <ricdelfin@bloomberg.net>
2025-06-12 06:43:43 -0400

    Add tab and spacing settings for a bunch of languages

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,14 @@
 -- Add any additional options here
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+
+-- Language-specific settings
+vim.cmd("autocmd Filetype c,h setlocal ts=8 sw=8 cindent")
+vim.cmd("autocmd Filetype cpp,hpp,cxx setlocal ts=2 sw=2 expandtab")
+vim.cmd("autocmd Filetype proto setlocal ts=2 sw=2 expandtab")
+vim.cmd("autocmd Filetype python setlocal ts=4 sw=4 expandtab")
+vim.cmd("autocmd Filetype go,html,javascript setlocal ts=2 sw=2 expandtab")
+vim.cmd("autocmd Filetype ruby setlocal ts=2 sw=2 expandtab")
+vim.cmd("autocmd Filetype rust setlocal ts=4 sw=4 expandtab")
+vim.cmd("autocmd Filetype yaml setlocal ts=2 sw=2 expandtab")
+vim.cmd("autocmd Filetype groovy setlocal ts=4 sw=4 expandtab")
